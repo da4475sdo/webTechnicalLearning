20f02546b996055cc692cb4535f2aa1ffaafb62d
180222 <180222>
2016-09-12 08:45:45 +0800

    web Technical Learning 2016-9-12 08:45:40

---
+++ webStormPro/JSON.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Administrator on 2016/8/20.
+ */
+var json={
+    "properties":["demoID","name"],
+    "condition":[
+        {
+            "relation":"and",
+            "name":"demoID",
+            "value":"111",
+            "operation":"=",
+            "isCombination":false,
+            "type":0,
+            "paramName":"demoID"
+        },
+        {
+            "name":"name",
+            "value":"zmk",
+            "operation":"=",
+            "isCombination":false,
+            "type":0,
+            "paramName":"name"
+        }
+    ],
+    "sortField":"demoID",
+    "order":"desc",
+    "pagination":1,
+    "limit":10,
+    "needLink":false
+}
+
